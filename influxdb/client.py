770ca82f2c07f36e16290d1edf97685202e7804f
vdecaro <valeriodc95@hotmail.it>
2022-04-02 19:15:51 +0200

    refactored with teaching decorator

--- influxdb/client.py
+++ influxdb/client.py
@@ -1,22 +1,33 @@
+import os
+
+from base.node import TEACHINGNode
 from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 
 class InfluxDBClientHandler:
 
-    def __init__(self, host, port, token, org, bucket):
-        self._host = host
-        self._port = port
-        self._token = token
-        self._org = org
-        self._bucket = bucket
+    def __init__(self):
+        self._host = os.environ['INFLUXDB_HOST']
+        self._port = int(os.environ['INFLUXDB_PORT'])
+        self._token = os.environ['INFLUXDB_TOKEN']
+        self._org = os.environ['INFLUXDB_ORG']
+        self._bucket = os.environ['INFLUXDB_BUCKET']
 
-        self._client = InfluxDBClient(url=f'http://{host}:{port}', token=token, org=org)
-        self._write_api = self._client.write_api(write_options=SYNCHRONOUS)
+        self._client = None
+        self._write_api = None
     
+    @TEACHINGNode(producer=False, consumer=True)
     def __call__(self, input_fn):
 
-        while True:
-            dp = next(input_fn)   
-            p = Point(dp.topic).field(dp.body.id, dp.body.value) # Specify the convention
+        for msg in input_fn: 
+            p = Point(msg.topic).field(msg.body.id, msg.body.value) # TODO: Specify the convention of messages
             self._write_api.write(bucket=self._bucket, record=p)
+
+    def _build(self):
+        self._client = InfluxDBClient(
+            url=f'http://{self._host}:{self._port}', 
+            token=self._token, 
+            org=self._org
+        )
+        self._write_api = self._client.write_api(write_options=SYNCHRONOUS)
--- main.py
+++ main.py
@@ -1,29 +1,8 @@
 import os
-
-from base.node import TEACHINGNode
-
-
-def get_service_fn():
-    SERVICE_TYPE = os.getenv('SERVICE_TYPE')
-    produce, consume = False, True
-
-    service_fns = []
-    if SERVICE_TYPE == 'INFLUXDB':
-        from influxdb import InfluxDBClientHandler
-        host = os.getenv('INFLUXDB_HOST')
-        port = int(os.getenv('INFLUXDB_PORT'))
-        bucket =os.getenv('INFLUXDB_BUCKET')
-        org = os.getenv('INFLUXDB_ORG')
-        token = os.getenv('INFLUXDB_TOKEN')
-
-        client = InfluxDBClientHandler(host, port, token, org, bucket)
-        service_fns.append(client)
-    
-    return service_fns, produce, consume
+from pydoc import locate
 
 
 if __name__ == '__main__':
-    service_fn, produce, consume = get_service_fn()
-    node = TEACHINGNode(service_fn, produce, consume)
-    node.build()
-    node.start()
+    service_class = locate(os.environ['SERVICE_TYPE'])
+    service = service_class()
+    service()
